8609bd92c5678c0fc944873bb202cd473f3d0696
unknown <vomanhdat10998@gmail.com>
2020-11-06 15:32:43 +0700

    custom_api

---
+++ src/ultity/Request.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { TOKEN } from "./ConfigWeb";
+export const callApi = (url = "", mathod = "", data = {}) => {
+  const config = {};
+  const token = localStorage.getItem(TOKEN);
+  if (token) {
+    config.headers = {
+      Authorization: "Bearer " + token,
+    };
+  }
+  return axios({
+    url: url,
+    method: method,
+    data: data,
+    ...config,
+  });
+};
+export default callApi;
