27002878db85b4cf1f89a87f46d02d202c64fa13
unknown <vomanhdat10998@gmail.com>
2020-11-12 19:20:39 +0700

    editSCSS_Login_SignUp_Form

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,30 +1,41 @@
-import React from 'react'
+import React from "react";
 import "./Login.scss";
-import { NavLink } from "react-router-dom"
+import { CloseCircleOutlined } from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
 
 export default function Login() {
   return (
-    <div className="dangNhap row">
-      <div className="col-3"></div>
-      <form className="container col-6">
-        <h3 className="display-4 text-center">Đăng Nhập </h3>
-        <div className="form-group">
-          <p> Tài Khoản </p>
-          <input name="userName" className="form-control"/>
-        </div>
-        <div className="form-group">
-          <p>  Mật Khẩu </p>
-          <input name="passWord" className="form-control"/>
-        </div>
-        <div className="form-group">
-          <button className="btn btn-success " type="submit"> Đăng Nhập </button>
-        </div>
-        <div className="form-group">
-          <p> Bạn chưa có tài khoản ? <NavLink to="/signup"> Đăng Ký </NavLink> </p>
-        </div>
-      </form>
-      <div className="col-3"></div>
+    <div className="login">
+      <div className="login-form">
+        <form>
+          <h3 className="text-center">Đăng nhập</h3>
+          <div className="form-group">
+            <p>Tài Khoản</p>
+            <input name="userName" className="form-control formLogin" />
+          </div>
+          <div className="form-group">
+            <p>Mật Khẩu</p>
+            <input
+              name="passWord"
+              type="password"
+              className="form-control formLogin"
+            />
+          </div>
+          <div className="form-group text-center mt-4">
+            <button className="btnLogin w-100 " type="submit">
+              Đăng Nhập
+            </button>
+          </div>
+          <div className="form-group text-center">
+            <p>
+              Bạn chưa có tài khoản ? <NavLink to="/signup">Đăng Ký</NavLink>
+            </p>
+          </div>
+        </form>
+        <NavLink to="/home" className="btnClose">
+          <CloseCircleOutlined />
+        </NavLink>
+      </div>
     </div>
-  )
+  );
 }
-
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,49 +1,63 @@
-import React from 'react';
+import React from "react";
+import "./SignUp.scss";
+import { CloseCircleOutlined } from "@ant-design/icons";
 import { NavLink } from "react-router-dom";
-import "./SignUp.scss"
 
 export default function SignUp() {
-    return (
-      <div className="dangKy row">
-        <div className="col-3"></div>
-        <form className="container col-6">
-          <h3 className="display-4 text-center">Đăng Ký </h3>
+  return (
+    <div className="login">
+      <div className="signUp-form">
+        <form>
+          <h3 className="text-center">Đăng ký tài khoản</h3>
           <div className="form-group">
-            <p> Họ Tên </p>
-            <input type="text" name="userName" className="form-control" placeholder="username" />
+            <p>Tài Khoản</p>
+            <input name="taiKhoan" className="form-control formSignUp" />
           </div>
           <div className="form-group">
-            <p> Tài Khoản </p>
-            <input type="text" name="account" className="form-control" placeholder="Account" />
+            <p>Tên người dùng</p>
+            <input name="hoTen" className="form-control formSignUp" />
           </div>
           <div className="form-group">
-            <p> Mật Khẩu </p>
-            <input type="password" name="passWord" className="form-control" placeholder="password" />
+            <p>Mật Khẩu</p>
+            <input
+              name="matKhau"
+              type="password"
+              className="form-control formSignUp"
+            />
           </div>
           <div className="form-group">
-            <p> Email </p>
-            <input type="email" name="email" className="form-control" placeholder="Enter email" />
+            <p>Email</p>
+            <input
+              name="email"
+              type="email"
+              className="form-control formSignUp"
+            />
           </div>
+
           <div className="form-group">
-            <p> Số Điện Thoại </p>
-            <input type="text" name="phonenumber" className="form-control" placeholder="Enter phone number" />
+            <p>Số điện thoại</p>
+            <input
+              name="soDt"
+              type="phoneNumber"
+              className="form-control formSignUp"
+            />
           </div>
-          <div className="form-group">
-            <p> Mã Nhóm </p>
-            <input type="text" name="maNhom" className="form-control" placeholder="Group ID" />
-          </div>
-          <div className="form-group">
-            <p> Mã Loại Người Dùng </p>
-            <input type="text" name="maLoaiNguoiDung" className="form-control" placeholder="ID User" />
+
+          <div className="form-group text-center mt-4">
+            <button className="btnSignUp w-100 " type="submit">
+              Đăng ký
+            </button>
           </div>
           <div className="form-group text-center">
-            <button className="btn btn-success" type="submit"> Đăng Ký </button>
-          </div>
-          <div className="form-group">
-            <p>Bạn đã có tài khoản ? <NavLink  to="/login"> Đăng Nhập</NavLink> </p>
+            <p>
+              Bạn đã có tài khoản ? <NavLink to="/login">Đăng nhập</NavLink>
+            </p>
           </div>
         </form>
-        <div className="col-3"></div>
+        <NavLink to="/home" className="btnClose">
+          <CloseCircleOutlined />
+        </NavLink>
       </div>
-    );
+    </div>
+  );
 }
